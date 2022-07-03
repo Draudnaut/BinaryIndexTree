5ac13aa0cd7be3f1600eaed489ce055f3cf5b9c8
henhimicons <henhimicons@qq.com>
2022-07-03 18:18:47 +0800

    construct method and Destructor

--- BinaryTreeNode.hpp
+++ BinaryTreeNode.hpp
@@ -8,20 +8,45 @@ namespace BinaryTreeNode
     private:
         K key;
         V value;
+        int depth;
+        BinaryTreeNode *left;
+        BinaryTreeNode *right;
 
     public:
         BinaryTreeNode(K key, V value)
         {
             this->key = key;
             this->value = value;
+            left = nullptr;
+            right = nullptr;
+            depth = 0;
+            std::cout << "constructor constructs object." << std::endl;
         }
 
-        void setKey(K newkey){
+        ~BinaryTreeNode()
+        {
+            delete[] left;
+            delete[] right;
+            std::cout << "deconstructor destroyed object." << std::endl;
+        }
+        void setKey(K newkey)
+        {
             key = newkey;
         }
 
-        void setvalue(V newvalue){
+        void setvalue(V newvalue)
+        {
             value = newvalue;
+        }
+
+        K getKey()
+        {
+            return key;
+        }
+
+        V getValue()
+        {
+            return value;
         }
     };
 }
